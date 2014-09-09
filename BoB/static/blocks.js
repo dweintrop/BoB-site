395cf4a44af9eed426d23b7777052ccfea789d4d
dweintrop <dnw882@dweintrop2-SESP.local>
2014-09-09 18:05:15 -0500

    fixing disappearing 0

--- BoB/static/blocks.js
+++ BoB/static/blocks.js
@@ -2564,7 +2564,7 @@ BlockMorph.prototype.mappedCode = function (definitions) {
 
     codeLines = code.split('\n');
     this.inputs().forEach(function (input) {
-        parts.push((input.mappedCode(defs)) ? input.mappedCode(defs).toString() : "");
+        parts.push((input.mappedCode(defs) || input.mappedCode(defs) == 0) ? input.mappedCode(defs).toString() : "");
     });
     parts.forEach(function (part) {
         var partLines = part.split('\n'),
