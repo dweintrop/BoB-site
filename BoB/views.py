dda8b4db66ba0c150f3a86b935767f480d840446
dweintrop <dnw882@dhcp-10-101-70-42.wireless.northwestern.private>
2014-09-23 16:01:03 -0500

    another filter on query for matching runs

--- BoB/views.py
+++ BoB/views.py
@@ -42,7 +42,8 @@ def snapRun(request):
 			)
 
 		# only save new record when edits have been made - else increment the NumRuns field
-		previousRunCount = SnapRun.objects.filter(StudentID=snapRun.StudentID, PairID=snapRun.PairID, ScriptXML=snapRun.ScriptXML).count()
+		last25hours = datetime.datetime.now() - datetime.timedelta(hours = 25)
+		previousRunCount = SnapRun.objects.filter(StudentID=snapRun.StudentID, PairID=snapRun.PairID, TimeStamp__gte=last25hours, ScriptXML=snapRun.ScriptXML).count()
 		if (previousRunCount > 0 and snapRun.RunType != 'projectClose'):
 			previousRun = SnapRun.objects.filter(StudentID=snapRun.StudentID, PairID=snapRun.PairID, ScriptXML=snapRun.ScriptXML).order_by("-TimeStamp").first()
 			previousRun.NumRuns += 1
