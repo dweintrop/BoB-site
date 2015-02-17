724dd9a6258eb392753f3d2190d4d15de119f12f
dweintrop <dnw882@dhcp-10-101-70-153.wireless.northwestern.private>
2015-02-17 13:36:53 -0600

    try 2000

--- oas/views.py
+++ oas/views.py
@@ -31,7 +31,7 @@ def view_xml(request):
 
 def fix_bad_xml(request):
 	count = 0
-	allRuns = SnapRun.objects.all()[3500:6500]
+	allRuns = SnapRun.objects.all()[3500:5500]
 	for run in allRuns:
 		if '" <variables' in run.ScriptXML:
 			run.ScriptXML = run.ScriptXML.replace('" <variables', '"><variables') 
